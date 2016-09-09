a8824566c4d9821229ce18059345860e8613b037
Isaias Tavares <isaiasengsoft@gmail.com>
2016-09-09 08:47:23 -0300

    Criando a classe Funcionario. Exemplo de Encapsulamento

---
+++ Funcionario.java
@@ -0,0 +1,22 @@
+public class Funcionario {
+
+	private String nome;
+	private Double salario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+}
