e1a01786a0c33cc0d9eac7950d4f3ef153e36c8d
Hiago Koziel Rahmig <hiagokoziel100@gmail.com>
2016-09-09 19:22:40 -0300

    Corrigindo método saltar

--- Exemplo Lebre/Depois da Refatoração/Corrida.java
+++ Exemplo Lebre/Depois da Refatoração/Corrida.java
@@ -1,12 +1,11 @@
 public class Corrida {
 
     
-    //Vari�vel que define se a corrida possui um vencedor
-    static boolean hasWinner = false;
-    Lebre lebre = new Lebre();
+    //Variável que define se a corrida possui um vencedor
+    private static boolean hasWinner = false;
     
-    //Método saltar utilizado pelas lebres
-    public void saltar() {
+    //MÃ©todo saltar utilizado pelas lebres
+    public void saltar(Lebre lebre) {
         while (hasWinner == false) {
             int pulo = 1 + (int) (Math.random() * ((3 - 1) + 1));
             lebre.distanciaPercorrida = lebre.distanciaPercorrida + pulo;
