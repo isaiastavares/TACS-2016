7a39bc5f4a5c5c2be4170b784d2c2a6bd07efe45
Hiago Koziel Rahmig <hiagokoziel100@gmail.com>
2016-09-09 15:44:20 -0300

    Update Lebre.java

--- Exemplo Lebre/Lebre.java
+++ Exemplo Lebre/Lebre.java
@@ -1,17 +1,21 @@
-package lebre;
-
+//Exemplo de uma classe que possui objeto do tipo Lebre, onde duas ou mais podem realizar uma corrida
 public class Lebre {
 
+    //Variáveis do objeto Lebre
     int saltos;
     int idLebre;
     int distanciaPercorrida;
+    
+    //Variável que define se a corrida possui um vencedor
     static boolean hasWinner = false;
 
+    //Construtor que verifica inicializa o valor de saltos e o id da lebre
     public Lebre(int saltos, int idLebre) {
         this.idLebre = idLebre;
         this.saltos = saltos;
     }
 
+    //Método saltar utilizado pelas lebres
     public void saltar() {
         while (hasWinner == false) {
             int pulo = 1 + (int) (Math.random() * ((3 - 1) + 1));
